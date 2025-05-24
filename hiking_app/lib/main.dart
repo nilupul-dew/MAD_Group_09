02fde258730f018281c1192e410c89d420d30388
W.A.S.C.N. Dewmini Ariyarathne <nilupul1025@gmail.com>
2025-05-24 15:21:31 +0530

    fix: merge conflict error fixed

--- hiking_app/lib/main.dart
+++ hiking_app/lib/main.dart
@@ -1,31 +1,38 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:hiking_app/firebase_options.dart';
 
-final db = FirebaseFirestore.instance;
-
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+  MyApp({super.key});
+  final FirebaseFirestore db = FirebaseFirestore.instance;
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("Firestore Example")),
+        appBar: AppBar(
+          title: Text("Firestore Example", style: TextStyle(color: Colors.red)),
+        ),
         body: Center(
           child: ElevatedButton(
             onPressed: () async {
-              await db.collection("test3").add({
+              await db.collection("test2").add({
                 "message": "Hello, Firestore!",
               });
             },
             child: Text("Add Document"),
           ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    ));
+        ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+    );
   }
 }
