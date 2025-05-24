54b6d37606bd29fac80edfdab01a7c57c4062137
Rusiru Dissanayaka <137480167+RusiruH23@users.noreply.github.com>
2025-05-24 09:56:48 +0530

    Revert "Firebase setup"

--- hiking_app/android/app/build.gradle.kts
+++ hiking_app/android/app/build.gradle.kts
@@ -3,13 +3,12 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-    id("com.google.gms.google-services")
 }
 
 android {
     namespace = "com.example.hiking_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -43,4 +42,3 @@ android {
 flutter {
     source = "../.."
 }
-
