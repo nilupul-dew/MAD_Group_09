5af5f80cecdb7c961bd4da36d84b0e2e5ce09799
D. L. Chamod Lakshitha <132426976+Chamod-2001@users.noreply.github.com>
2025-05-26 14:45:55 +0530

    Add PlaceModel class to represent hiking and campsite data

---
+++ hiking_app/lib/domain/models/place_model.dart
@@ -0,0 +1,43 @@
+class PlaceModel {
+  final String name;
+  final String locationName;
+  final String imageUrl;
+  final String type;
+  final String category;
+  final String description;
+  final List<String> suitableMonths;
+
+  PlaceModel({
+    required this.name,
+    required this.locationName,
+    required this.imageUrl,
+    required this.type,
+    required this.category,
+    required this.description,
+    required this.suitableMonths,
+  });
+
+  factory PlaceModel.fromJson(Map<String, dynamic> json) {
+    return PlaceModel(
+      name: json['name'] ?? '',
+      locationName: json['locationName'] ?? '',
+      imageUrl: json['imageUrl'] ?? '',
+      type: json['type'] ?? '',
+      category: json['category'] ?? '',
+      description: json['description'] ?? '',
+      suitableMonths: List<String>.from(json['suitableMonths'] ?? []),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'locationName': locationName,
+      'imageUrl': imageUrl,
+      'type': type,
+      'category': category,
+      'description': description,
+      'suitableMonths': suitableMonths,
+    };
+  }
+}
