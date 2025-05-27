99a00cd96e762c54d32b099398fb9575ae7b7d25
D. L. Chamod Lakshitha <132426976+Chamod-2001@users.noreply.github.com>
2025-05-27 15:25:25 +0530

    Add support for new fields in FirestorePlaceService (province, district, images, videoUrl, location)

--- hiking_app/lib/data/firebase_services/firestore_place_service.dart
+++ hiking_app/lib/data/firebase_services/firestore_place_service.dart
@@ -1,3 +1,36 @@
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:hiking_app/domain/models/place_model.dart';
+
+// class FirestorePlaceService {
+//   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+//   Future<List<PlaceModel>> getAllPlaces() async {
+//     final snapshot = await _firestore.collection('places').get();
+//     return snapshot.docs.map((doc) => PlaceModel.fromJson(doc.data())).toList();
+//   }
+
+//   Future<List<PlaceModel>> searchPlaces(String query) async {
+//     final snapshot =
+//         await _firestore
+//             .collection('places')
+//             .where('name', isGreaterThanOrEqualTo: query)
+//             .where('name', isLessThanOrEqualTo: query + '\uf8ff')
+//             .get();
+
+//     return snapshot.docs.map((doc) => PlaceModel.fromJson(doc.data())).toList();
+//   }
+
+//   Future<List<PlaceModel>> filterPlacesByCategory(String category) async {
+//     final snapshot =
+//         await _firestore
+//             .collection('places')
+//             .where('category', isEqualTo: category)
+//             .get();
+
+//     return snapshot.docs.map((doc) => PlaceModel.fromJson(doc.data())).toList();
+//   }
+// }
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:hiking_app/domain/models/place_model.dart';
 
@@ -6,7 +39,11 @@ class FirestorePlaceService {
 
   Future<List<PlaceModel>> getAllPlaces() async {
     final snapshot = await _firestore.collection('places').get();
-    return snapshot.docs.map((doc) => PlaceModel.fromJson(doc.data())).toList();
+
+    return snapshot.docs.map((doc) {
+      final data = doc.data();
+      return PlaceModel.fromJson(data);
+    }).toList();
   }
 
   Future<List<PlaceModel>> searchPlaces(String query) async {
@@ -28,5 +65,17 @@ class FirestorePlaceService {
             .get();
 
     return snapshot.docs.map((doc) => PlaceModel.fromJson(doc.data())).toList();
+  }
+
+  Future<void> addPlace(PlaceModel place) async {
+    await _firestore.collection('places').add(place.toJson());
+  }
+
+  Future<void> updatePlace(String docId, PlaceModel place) async {
+    await _firestore.collection('places').doc(docId).update(place.toJson());
+  }
+
+  Future<void> deletePlace(String docId) async {
+    await _firestore.collection('places').doc(docId).delete();
   }
 }
