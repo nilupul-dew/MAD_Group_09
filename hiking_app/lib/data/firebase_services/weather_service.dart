1ecc64316c94a9796c9fe0d2e3ca6a209fdad906
D. L. Chamod Lakshitha <132426976+Chamod-2001@users.noreply.github.com>
2025-05-27 22:23:08 +0530

    Add WeatherService class to fetch weather data from OpenWeatherMap API

---
+++ hiking_app/lib/data/firebase_services/weather_service.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+import 'package:hiking_app/domain/models/weather_model.dart';
+
+class WeatherService {
+  final String apiKey = 'YOUR_OPENWEATHERMAP_API_KEY';
+
+  Future<Weather> fetchWeather(double lat, double lon) async {
+    final url = Uri.parse(
+      'https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&units=metric&appid=$apiKey',
+    );
+
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      return Weather.fromJson(data);
+    } else {
+      throw Exception('Failed to load weather');
+    }
+  }
+}
