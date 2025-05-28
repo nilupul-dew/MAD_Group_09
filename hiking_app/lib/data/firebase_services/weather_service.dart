953c794ffbbaf0d8280e2aadf6906c3e2d3eca6d
D. L. Chamod Lakshitha <132426976+Chamod-2001@users.noreply.github.com>
2025-05-29 00:46:06 +0530

    Extend WeatherService to fetch both current weather and 3-day forecast using OpenWeatherMap API

--- hiking_app/lib/data/firebase_services/weather_service.dart
+++ hiking_app/lib/data/firebase_services/weather_service.dart
@@ -1,10 +1,11 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-//import 'package:hiking_app/domain/models/weather_model.dart';
+import 'package:hiking_app/domain/models/weather_model.dart';
 
 class WeatherService {
   final String apiKey = '2a8c6b6a7cfda04afbc3645345e1c03a';
 
+  /// Get current weather for a given location
   Future<Weather> fetchWeather(double lat, double lon) async {
     final url = Uri.parse(
       'https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&units=metric&appid=$apiKey',
@@ -19,9 +20,35 @@ class WeatherService {
       throw Exception('Failed to load weather');
     }
   }
+
+  /// Get 3-day forecast for the given location
+  Future<List<WeatherForecast>> fetchForecast(double lat, double lon) async {
+    final url = Uri.parse(
+      'https://api.openweathermap.org/data/2.5/forecast?lat=$lat&lon=$lon&units=metric&appid=$apiKey',
+    );
+
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+
+      final List<dynamic> forecastList = data['list'];
+
+      // Select one forecast per day (every 8th item = 24 hours)
+      List<WeatherForecast> dailyForecasts = [];
+      for (int i = 0; i < forecastList.length; i += 8) {
+        final item = forecastList[i];
+        dailyForecasts.add(WeatherForecast.fromJson(item));
+      }
+
+      return dailyForecasts.take(3).toList(); // Only 3-day forecast
+    } else {
+      throw Exception('Failed to load forecast');
+    }
+  }
 }
 
-//---------------------------------------//
+/// For current weather
 class Weather {
   final String description;
   final double temperature;
